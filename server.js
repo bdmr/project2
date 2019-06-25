026f2f3bcc009b0a688cd29c58164041faf63fee
Maggie Jo Saylor <saylor.mj@gmail.com>
2019-06-24 21:29:20 -0500

    this server needs to be fixed :P

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ app.use(express.static(__dirname + '/public')); // you should change this to be 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
+// require("./routes/api-routes.js")(app);
+require("./public/routes/html-routes.js")(app);
+
 app.listen(port);
 console.log("App listening on PORT 9000");
 
