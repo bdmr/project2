9dff8fff05353cb6777749e65c2e5189e78e1b29
tarikac <tarika.common@gmail.com>
2019-07-01 20:58:09 -0500

    fixed config files

--- server.js
+++ server.js
@@ -4,34 +4,33 @@
 // ******************************************************************************
 // *** Dependencies
 // =============================================================
+
+require('dotenv').config();
 var express = require("express");
 
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = process.env.PORT || 8080;
+var port = process.env.PORT || 9000;
+//var config = require('./config');
 
 // Requiring our models for syncing
-var db = require("./models");
+//var db = require("./models");
 
 // Sets up the Express app to handle data parsing
+app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-// Static directory
-app.use(express.static("public"));
 
 // Routes
 // =============================================================
 require("./routes/html-routes.js")(app);
 require("./routes/parent-api-routes.js")(app);
-require("./routes/post-api-routes")(app);
+require("./routes/post-api-routes.js")(app);
 require("./routes/teacher-api-routes.js")(app);
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
-db.sequelize.sync({ force: true }).then(function() {
-  app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
-});
+
+  app.listen(port);
+  console.log("App listening on PORT 9000");
