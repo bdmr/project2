af4368152b349bf1c284e1d5019e617fdd8a430f
Maggie Jo Saylor <saylor.mj@gmail.com>
2019-07-01 21:14:00 -0500

    updated teacher.js & parent.js

--- models/parent.js
+++ models/parent.js
@@ -1,16 +1,83 @@
-module.exports = function(sequelize, DataTypes){
-    var Parent = sequelize.define("Parent", {
-        parent_name: DataTypes.STRING
+$(document).ready(function(){
+
+    var nameInput = $("#parent_name");
+    var parentList = $("#parentdivbody");
+    var parentContainer = $(".parent-container");
+
+
+$(document).on("submit", "#parent-form", handleParentFormSubmit);
+$(document).on("click", ".delete-parent", handleDeleteButton);
+
+getParent();
+
+function handleParentFormSubmit(event){
+    event.preventDefault();
+
+    if(!nameInput.val().trim().trim()){
+        return;
+    }
+
+    upsertParent ({
+        parent_name: nameInput.val().trim()
     });
-
-    Parent.associate = function(models){
-
-        Parent.hasMany(models.Post, {
-            onDelete: "cascade"
-        });
-    };
-    return Parent;
-
-
-
 }
+
+function upsertParent(parentData){
+    $.post("/api/parents", parentData)
+    .then(getParent);
+}
+
+//create a new list of teachers
+
+function createParentRow(parentData){
+    var newParentDiv = $("<div>");
+    var newParentName = $("<p>");
+    newParentName.data("parent", parentData);
+    newParentName.append(parentData.parent_name);
+    newParentDiv.append(newParentName);
+    newParentDiv.append("<div><a class='delete-parent'> Delete Parent</a></div>");
+    return newParentDiv;
+}
+
+//retrieve teacher, get them ready to render on page
+function getParent(){
+    $.get("/api/parents", function(data){
+        var rowsToAdd = [];
+        for (var i = 0; i < data.length; i++){
+            rowsToAdd.push(createParentRow(data[i]));
+        }
+        renderParentList(rowsToAdd);
+        nameInput.val("");
+    });
+}
+
+//render list of authors
+function renderParentList(rows){
+    parentList.children().not(":last").remove();
+    parentContainer.children(".alert").remove();
+    if(rows.length){
+        console.log(rows);
+        parentList.prepend(rows);
+    }
+    else {
+        renderEmpty();
+    }
+}
+
+//display when there are no teachers
+function renderEmpty() {
+    var pleaseCreate = $("<div>");
+    pleaseCreate.text("Please create a Parent");
+    teacherContainer.append(pleaseCreate);
+}
+
+function handleDeleteButton() {
+    var listItemData = $(this).parent("div").data("parent");
+    var id = listItemData.id;
+    $.ajax({
+        method: "DELETE",
+        url: "/api/parents/" + id
+    }).then(getParent);
+}
+
+});
--- models/teacher.js
+++ models/teacher.js
@@ -1,14 +1,82 @@
-module.exports = function(sequelize, DataTypes){
-    var Teacher = sequelize.define("Teacher", {
-        teacher_name: DataTypes.STRING
+$(document).ready(function(){
+
+    var nameInput = $("#teacher_name");
+    var teacherList = $("#divbody");
+    var teacherContainer = $(".teacher-container");
+
+
+$(document).on("submit", "#teacher-form", handleTeacherFormSubmit);
+$(document).on("click", ".delete-teacher", handleDeleteButton);
+
+getTeachers();
+
+function handleTeacherFormSubmit(event){
+    event.preventDefault();
+
+    if(!nameInput.val().trim().trim()){
+        return;
+    }
+
+    upsertTeacher ({
+        teacher_name: nameInput.val().trim()
     });
+}
 
-    Teacher.associate = function(models){
-        Teacher.hasMany(models.Post, {
-            onDelete: "cascade"
-        });
-    };
-    return Teacher;
+function upsertTeacher(teacherData){
+    $.post("/api/teachers", teacherData)
+    .then(getTeachers);
+}
 
+//create a new list of teachers
 
-};
+function createTeacherRow(teacherData){
+    var newTeacherDiv = $("<div>");
+    var newTeacherName = $("<p>");
+    newTeacherName.data("teacher", teacherData);
+    newTeacherName.append(teacherData.teacher_name);
+    newTeacherDiv.append(newTeacherName);
+    newTeacherDiv.append("<div><a class='delete-teacher'> Delete Teacher</a></div>");
+    return newTeacherDiv;
+}
+
+//retrieve teacher, get them ready to render on page
+function getTeachers(){
+    $.get("/api/teachers", function(data){
+        var rowsToAdd = [];
+        for (var i = 0; i < data.length; i++){
+            rowsToAdd.push(createTeacherRow(data[i]));
+        }
+        renderTeacherList(rowsToAdd);
+        nameInput.val("");
+    });
+}
+
+function renderTeacherList(rows){
+    teacherList.children().not(":last").remove();
+    teacherContainer.children(".alert").remove();
+    if(rows.length){
+        console.log(rows);
+        teacherList.prepend(rows);
+    }
+    else {
+        renderEmpty();
+    }
+}
+
+//display when there are no teachers
+function renderEmpty() {
+    var pleaseCreate = $("<div>");
+    pleaseCreate.text("Please create a Teacher.");
+    teacherContainer.append(pleaseCreate);
+}
+
+function handleDeleteButton() {
+    var listItemData = $(this).parent("div").data("teacher");
+    var id = listItemData.id;
+    $.ajax({
+        method: "DELETE",
+        url: "/api/teachers/" + id
+    }).then(getTeachers);
+}
+
+});
