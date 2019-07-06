d3333bca3d46cfea139cca643f0c121eb297e280
tarikac <tarika.common@gmail.com>
2019-07-05 19:58:30 -0500

    updates to the calendar

--- public/js/script.js
+++ public/js/script.js
@@ -6,7 +6,7 @@
 				center: 'title',
 				right: 'month,basicWeek,basicDay'
 			},
-			defaultDate: '2019-07-04',
+			defaultView: 'month',
 			navLinks: true, // can click day/week names to navigate views
 			editable: true,
 			eventLimit: true, // allow "more" link when too many events
@@ -27,12 +27,7 @@
 				{
 					id: 999,
 					title: 'Summer Program',
-					start: '2016-07-24T16:00:00'
-				},
-				{
-					title: 'Conference',
-					start: '2016-12-11',
-					end: '2016-12-13'
+					start: '2019-07-24T16:00:00'
 				},
 				{
 					title: 'Meeting with Parents',
@@ -40,29 +35,49 @@
 					end: '2019-07-08T12:30:00'
 				},
 				{
-					title: 'Click for Google',
-					url: 'https://google.com/',
-					start: '2019-07-28'
+					title: 'LEGO Brick Play',
+					url: 'https://www.mortonarb.org/events/lego%C2%AE-brick-play-0',
+					start: '2019-07-28T11:00:00',
+					end: '2019-07-28T16:00:00',
+					color: '#20677F',
+
 				},
 				{
 					title: 'Kids Slime Workshop',
 					url: 'https://www.artstudiolocalcolor.com/',
 					start: '2019-07-06',
-					end: '2019-07-06'
+					end: '2019-07-06',
+					color: '#d84c5d'
+				},
+				{
+					title: 'Project Presentations',
+					url: 'https://sps.northwestern.edu/',
+					start: '2019-07-06T10:00:00',
+					end: '2019-07-06T14:00:00',
+					color: '#a90b22'
 				},
 				{
 					title: 'CCM Free Day',
-					url: 'chicagochildrensmuseum.org',
+					url: 'https://chicagochildrensmuseum.org',
 					start: '2019-07-07',
-					end: '2019-07-07'
+					end: '2019-07-07',
+					color: '#163912'
 				},
 				{
 					title: 'Parents Survival Night',
 					url: 'https://www.thelittlegym.com',
 					start: '2019-07-12',
-					end: '2019-07-12'
+					end: '2019-07-12',
+					color: 'orange'
 				}
-			]
+			],
+			eventClick: function(event){
+				if(event.url){
+					window.open(event.url, "_blank");
+					return false;
+				}
+			}
+		
 		});
 		
 	});
